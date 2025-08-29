f86977b47a02228a9a545cc357ebe4d68e620067
ACT-STUDENT <ACT-STUDENT@LAB3-PC15>
2025-08-29 14:15:44 +0800

    exercise1

---
+++ Camajalan_CaO/Camajalan_CaO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_CaO
+{
+    internal class Person
+    {
+        //Class Members
+        public string name;
+        public int age;
+        public string gender;
+
+        //Method to Display the fields
+        public void Greet()
+        {
+            Console.WriteLine("Good Day everyone, I'm " + name + " " + age + "years old. I'm a " + gender + ".");
+        }
+    }
+}
---
+++ Camajalan_CaO/Camajalan_CaO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_CaO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aivy Camajalan_IT306
+            //Title: Creating a Class
+
+            //Instantiate object
+            Person person1 = new Person();
+
+            //Initialization of fields
+            person1.name = "Aivy";
+            person1.age = 18;
+            person1.gender = "girl";
+
+            //call the method to display information
+            person1.Greet();
+
+            Console.ReadLine();
+        }
+    }
+    
+}
